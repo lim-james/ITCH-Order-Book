e4725c873044a8698fcc2b094cbe9c8f8ea03e25
james <jameslimbj@gmail.com>
2026-06-08 15:54:46 +0800

    rename enums to standardise screaming snake case

--- src/feed/file_source.cpp
+++ src/feed/file_source.cpp
@@ -27,14 +27,15 @@ FileSource::~FileSource() {
 }
 
 ReadResult FileSource::next() {
-    if (ptr_.empty()) return std::unexpected{DataReadFailure::StreamClosed};
-    if (ptr_.size() < sizeof(nasdaq::PacketSize)) return std::unexpected{DataReadFailure::InvalidFormat};
+    if (ptr_.empty()) return std::unexpected{DataReadFailure::STREAM_CLOSED};
+    if (ptr_.size() < sizeof(nasdaq::PacketSize)) return std::unexpected{DataReadFailure::INVALID_FORMAT};
 
     nasdaq::PacketSize packet_size{};
     std::memcpy(&packet_size, ptr_.data(), sizeof(nasdaq::PacketSize));
+
     ptr_ = ptr_.subspan(sizeof(nasdaq::PacketSize));
 
-    if (ptr_.size() < packet_size) return std::unexpected{DataReadFailure::InvalidFormat};
+    if (ptr_.size() < packet_size) return std::unexpected{DataReadFailure::INVALID_FORMAT};
 
     std::span data_frame{ptr_.data(), packet_size};
     ptr_ = ptr_.subspan(packet_size);
