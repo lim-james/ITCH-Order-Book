9cf43d5224a3d47eb60298ca77ab8e2aa9b2893d
james <jameslimbj@gmail.com>
2026-06-08 10:47:48 +0800

    connect feed handler to spsc

--- bench/replay.cpp
+++ bench/replay.cpp
@@ -1,16 +1,20 @@
 #include "feed/file_source.h"
 #include "feed/feed_handler.h"
+#include "spsc/spsc_factory.h"
 
 #include <print>
+#include <utility>
 
 int main(int argsc, char** argsv) {
     if (argsc <= 1) {
         std::println("Specify backtest data filepath.");
         return 0;
     }
+
+    auto spsc = make_spsc<std::byte, 1024>();
     
     FileSource  file_source{argsv[1]};
-    FeedHandler feed_handler{file_source};
+    FeedHandler feed_handler{file_source, std::move(spsc.producer)};
 
     feed_handler.poll();
     feed_handler.poll();
--- include/feed/feed_handler.h
+++ include/feed/feed_handler.h
@@ -1,25 +1,36 @@
 #pragma once 
 
 #include "data_source.h"
+#include "spsc/spsc_queue.h"
 
+#include <cstddef>
 #include <print>
 
-template<data_source source_t>
+template<data_source source_t, std::size_t N>
 class FeedHandler {
+    
+    using producer_t = SPSCProducer<std::byte, N>;
+    
 public:
 
-    FeedHandler(const source_t& source): source_(source) {}
+    FeedHandler(const source_t& source, producer_t&& producer)
+        : source_{source}
+        , producer_queue_{std::forward<producer_t&&>(producer)} {}
 
     void poll() {
         if (auto next_frame = source_.next()) {
-            const auto message_type = *reinterpret_cast<const char*>(next_frame->data());
-            std::println("{}", message_type);
+            auto c = *reinterpret_cast<const char*>(next_frame->data());
+            std::println("Message Type: {}", c);
+            while (producer_queue_.try_push_many(*next_frame) != PushResponse::SUCCESS) {
+                // std::println("Failed to push {} bytes", next_frame->size());
+            }
+            std::println("Read {} bytes", next_frame->size());
         }
     }
 
 private:
 
     source_t source_;
-
+    producer_t producer_queue_;
 
 };
