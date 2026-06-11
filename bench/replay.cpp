05db345723d5edf339f1c028318660fe372d0dc7
james <jameslimbj@gmail.com>
2026-06-11 18:25:39 +0800

    refactor benchmark to use multi thread symbol workers

--- bench/replay.cpp
+++ bench/replay.cpp
@@ -1,4 +1,4 @@
-#include "spsc/spsc_factory.h"
+#include "spmc/spmc_factory.h"
 #include "feed/file_source.h"
 #include "feed/feed_handler.h"
 #include "market_data/market_data_handler.h"
@@ -6,6 +6,19 @@
 #include <print>
 #include <utility>
 #include <thread>
+#include <type_traits>
+
+template<auto Stock, std::size_t Consumer_Id, typename spmc_t>
+auto create_worker(spmc_t& spmc) {
+    return [&] {
+        OrderBook order_book{};
+        using consumer_t = std::remove_reference_t<decltype(spmc.consumers[Consumer_Id])>;
+        MarketDataHandler<Stock, consumer_t> market_data_handler{
+            std::move(spmc.consumers[Consumer_Id]), std::move(order_book)
+        };
+        while (market_data_handler.poll());
+    };
+}
 
 int main(int argsc, char** argsv) {
     if (argsc <= 1) {
@@ -14,19 +27,15 @@ int main(int argsc, char** argsv) {
     }
 
     static constexpr std::size_t BUFFER_SIZE = 1 << 20; 
-    auto spsc = make_spsc<std::byte, BUFFER_SIZE>();
+    auto spmc = make_spmc<std::byte, BUFFER_SIZE>(3);
 
     auto feed_worker = std::jthread([&] {
         FileSource  file_source{argsv[1]};
-        FeedHandler feed_handler{file_source, std::move(spsc.producer)};
+        FeedHandler feed_handler{file_source, std::move(spmc.producer)};
         while (feed_handler.poll());
     });
 
-    auto book_worker = std::jthread([&] {
-        OrderBook order_book{};
-        MarketDataHandler<nasdaq::make_ticker("TSLA"), BUFFER_SIZE> market_data_handler{
-            std::move(spsc.consumer), std::move(order_book)
-        };
-        while (market_data_handler.poll());
-    });
+    auto tsla_worker = std::jthread(create_worker<nasdaq::make_ticker("TSLA"), 0>(spmc));
+    auto gold_worker = std::jthread(create_worker<nasdaq::make_ticker("GOLD"), 1>(spmc));
+    auto hsbc_worker = std::jthread(create_worker<nasdaq::make_ticker("HSBC"), 2>(spmc));
 }
